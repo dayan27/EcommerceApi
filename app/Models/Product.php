cbb7151738421721e0a93374735ba1470d43bd9c
dayan27 <dayanyenesew@gmail.com>
2021-06-25 10:05:16 +0300

    relationship created between Product and Review

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,4 +11,7 @@ class Product extends Model
     protected $fillable=[
         'name','detail','price','stock','discount'
     ];
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,4 +12,7 @@ class Review extends Model
     protected $fillable=[
         'product_id','customer','review','star'
     ];
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
